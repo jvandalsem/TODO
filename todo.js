2f722cb786d60b986f35fdf508e5f7e274c47a1f
John Van Dalsem <jjvandal@umich.edu>
2022-03-21 16:51:40 -0700

    added completion, still hacky with sizes

--- todo.js
+++ todo.js
@@ -3,8 +3,10 @@ $('.adder').on('click',()=>{
 						   <label for=\"check\"></label>
 						   <input type=\"checkbox\" class=\"check\"/>
 						   <p>${$('.todo-input')[0].value}</p>
-					   </div>`).on('click',function(){$(this)[0].classList.toggle('complete')})
-				
+					   </div>`)
+	$todoItem[0].childNodes[3].addEventListener('click',function(){
+		$(this)[0].parentElement.classList.toggle('complete')
+	})
 	$('.todo-list').append($($todoItem))
 	$('.todo-input')[0].value = ''
 	$('.todo-input')[0].focus()
