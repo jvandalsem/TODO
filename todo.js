700e964d3da3c5a85f01fe105b70658fbdbe8990
John Van Dalsem <jjvandal@umich.edu>
2022-03-22 16:45:54 -0700

    added delete task

--- todo.js
+++ todo.js
@@ -4,32 +4,40 @@ const validate = () =>{
 }
 
 $('.adder').on('click',()=>{
+	var $todoItem = $(`<div class=\"todo-item\">
+						       <label for=\"check\"></label>
+							   <input type=\"checkbox\" class=\"check\"/>
+							   <p>${$('.todo-input')[0].value}</p>
+							   <i class="material-icons-outlined" style="font-size:16px;display:none">delete</i>
+							   <span class="material-icons-outlined" style="font-size:16px;display:none">edit</span>
+						   </div>`)
+						   
+	$todoItem[0].children[1].addEventListener('click',function(){
+		$(this)[0].parentElement.classList.toggle('complete')
+		$todoItem[0].children[3].style.display = 'none'
+		$todoItem[0].children[4].style.display = 'none'
+	})
+	
+	$todoItem.on('mouseover',function(){
+		if($('.check').prop('checked')===true){
+			console.log('checked')
+		}
+		else{
+			$(this)[0].children[3].style.display = 'block'
+			$(this)[0].children[4].style.display = 'block'
+		}
+	})
+	
+	$todoItem.on('mouseout',function(){
+		$(this)[0].children[3].style.display = 'none'
+		$(this)[0].children[4].style.display = 'none'
+	})
+	
+	$todoItem[0].children[3].addEventListener('click',function(){
+		$todoItem[0].parentElement.removeChild($todoItem[0])
+	})
+	
 	if(validate()){
-		var $todoItem = $(`<div class=\"todo-item\">
-					   <label for=\"check\"></label>
-					   <input type=\"checkbox\" class=\"check\"/>
-					   <p>${$('.todo-input')[0].value}</p>
-					   <i class="material-icons-outlined" style="font-size:16px;display:none">delete</i>
-					   <span class="material-icons-outlined" style="font-size:16px;display:none">edit</span>
-				   </div>`)
-		$todoItem[0].childNodes[3].addEventListener('click',function(){
-			$(this)[0].parentElement.classList.toggle('complete')
-			$todoItem[0].children[3].style.display = 'none'
-			$todoItem[0].children[4].style.display = 'none'
-		})
-		$todoItem.on('mouseover',function(){
-			if($('.check').prop('checked')===true){
-				console.log('checked')
-			}
-			else{
-				$(this)[0].children[3].style.display = 'block'
-				$(this)[0].children[4].style.display = 'block'
-			}
-		})
-		$todoItem.on('mouseout',function(){
-			$(this)[0].children[3].style.display = 'none'
-			$(this)[0].children[4].style.display = 'none'
-		})
 		$('.todo-list').append($($todoItem))
 		$('.todo-input')[0].value = ''
 		$('.todo-input')[0].focus()
